da5016498e7f8166fbc636a4f702e42f8c2e63ae
leomoraiscam <leozonnn@gmail.com>
2020-10-30 21:11:25 -0300

    intregation api

--- Bootcamp GoStack 14/Mobile/ConceptsReactNative/src/index.js
+++ Bootcamp GoStack 14/Mobile/ConceptsReactNative/src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import api from './services/api';
+import {SafeAreaView, Text, FlatList, StyleSheet} from 'react-native';
 
 function App() {
-  return <View />;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={projects}
+        key={(project) => project.id}
+        renderItem={({item}) => (
+          <Text style={styles.project}>{item.title}</Text>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+  project: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
 
 export default App;
