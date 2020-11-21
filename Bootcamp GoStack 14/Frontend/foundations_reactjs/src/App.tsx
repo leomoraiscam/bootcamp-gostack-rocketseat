e5165c85f46612cf01aa648a6741647ac4c2dc4b
leomoraiscam <leozonnn@gmail.com>
2020-11-21 15:38:03 -0300

    implementatio styles with styled-components and global styles

--- Bootcamp GoStack 14/Frontend/foundations_reactjs/src/App.tsx
+++ Bootcamp GoStack 14/Frontend/foundations_reactjs/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Routes from './routes';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <Routes />
+  <>
+    <Routes />
+    <GlobalStyle />
+  </>
 );
 
 export default App;
