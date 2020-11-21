fc7399daea4f47e848cf1558103bafd5abcce94a
leomoraiscam <leozonnn@gmail.com>
2020-11-21 15:10:48 -0300

    configuration navigation with react-router-dom in app and resolve bugfixes

--- Bootcamp GoStack 14/Frontend/foundations_reactjs/src/App.tsx
+++ Bootcamp GoStack 14/Frontend/foundations_reactjs/src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import Routes from './routes';
 
-function App() {
-  return (
-    <h1>Ok</h1>
-  );
-}
+const App: React.FC = () => (
+  <Routes />
+);
 
 export default App;
