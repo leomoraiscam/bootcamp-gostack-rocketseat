c957668d1143609488ad9c87e560820f9fce278b
leomoraiscam <leozonnn@gmail.com>
2020-11-21 21:23:58 -0300

    styles of dashboard page

--- Bootcamp GoStack 14/Frontend/foundations_reactjs/src/pages/Dashboard/index.tsx
+++ Bootcamp GoStack 14/Frontend/foundations_reactjs/src/pages/Dashboard/index.tsx
@@ -1,6 +1,36 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form action="">
+      <input type="text" placeholder="Digite aqui..." />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="/test">
+        <img
+          src="https://avatars0.githubusercontent.com/u/49538119?s=460&u=39a6291923942b4f7cc8fcb4bce259d116807701&v=4"
+          alt="Leonardo"
+        />
+
+        <div>
+          <strong>tdd</strong>
+          <p>Test Development Drive</p>
+        </div>
+
+        <FiChevronRight size={20} />
+
+      </a>
+
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
